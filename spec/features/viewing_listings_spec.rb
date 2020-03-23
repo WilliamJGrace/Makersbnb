89af7342b6353586d29898ac7011e1c939fa8a5e
Peter Stevens <Peterstevens510@gmail.com>
2020-03-23 16:00:21 +0000

    viewing listings spec

--- spec/features/viewing_listings_spec.rb
+++ spec/features/viewing_listings_spec.rb
@@ -5,5 +5,15 @@ feature 'Viewing listings' do
     expect(page).to have_content "Lovely house"
     expect(page).to have_content "Romantic get-away"
     expect(page).to have_content "Seaside cottage"
+    expect(page).to have_content "Pandemic and chill"
   end
 end
+
+
+
+# @listings = [
+#   "Lovely house",
+#   "Romantic get-away",
+#   "Seaside cottage", 
+#   "Pandemic and chill"
+# ]
