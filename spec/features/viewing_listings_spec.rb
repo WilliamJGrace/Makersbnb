ae62d4070c60d56394ef7659024114e054f168a9
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-03-24 18:14:54 +0000

    changed viewing listings spec

--- spec/features/viewing_listings_spec.rb
+++ spec/features/viewing_listings_spec.rb
@@ -1,12 +1,11 @@
 feature 'Viewing listings' do
   scenario 'A user can see listings' do
-    visit('/listings')
     user = User.create(email: "test@makers.com", name: "will", username: "will123", password: "password456")
-    listing = Listing.create(user_id: user.id, name: "Peter Stevens", description: "Lovely house", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
-    listing = Listing.create(user_id: user.id, name: "Peter Stevens", description: "Romantic get-away", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
-    listing = Listing.create(user_id: user.id, name: "Peter Stevens", description: "Seaside cottage", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
-    listing = Listing.create(user_id: user.id, name: "Peter Stevens", description: "Pandemic and chill", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
-
+    Listing.create(user_id: user.id, name: "Peter Stevens", description: "Lovely house", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
+    Listing.create(user_id: user.id, name: "Peter Stevens", description: "Romantic get-away", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
+    Listing.create(user_id: user.id, name: "Peter Stevens", description: "Seaside cottage", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
+    Listing.create(user_id: user.id, name: "Peter Stevens", description: "Pandemic and chill", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
+    visit('/listings')
     expect(page).to have_content "Lovely house"
     expect(page).to have_content "Romantic get-away"
     expect(page).to have_content "Seaside cottage"
