e837187cc9ea7ccdee8214ca8a2e79a7d4622c1a
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-03-25 17:51:22 +0000

    trying to fix a bug

--- spec/features/requesting_booking_spec.rb
+++ spec/features/requesting_booking_spec.rb
@@ -1,7 +1,7 @@
 feature 'requesting booking' do
   scenario 'A user can request a booking' do
     user = User.create(email: "test@makers.com", name: "will", username: "will123", password: "password456")
-    Listing.create(user_id: user.id, name: "Peter Stevens", description: "Lovely house", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
+    Listing.create(user_id: user.id, name: "Peter Stevens", description: "Lovely house", price: 700, dates_available: "2020/03/31")
     visit '/login'
     fill_in(:username, with: 'will123')
     fill_in(:password, with: 'password456')
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -6,7 +6,7 @@ describe Requests do
   describe "#create" do
     it "can create a new request" do
       user = User.create(email: "test@makers.com", name: "will", username: "will123", password: "password456")
-      listing = Listing.create(user_id: user.id, name: "Peter Stevens", description: "Pandemic and chill, £100.00 a night", price: 700, date_created: "2020/03/24", dates_available: "2020/03/31")
+      listing = Listing.create(user_id: user.id, name: "Peter Stevens", description: "Pandemic and chill, £100.00 a night", price: 700, dates_available: "2020/03/31")
       request = Requests.create(listing_user_id: user.id, requester_user_id: user.id, listing_id: listing.id, name: listing.name, description: listing.description, price: listing.price, dates_booked: '01/01/01 - 02/02/02')
       expect(request).to be_a Requests
       expect(request.listing_user_id).to eq user.id
