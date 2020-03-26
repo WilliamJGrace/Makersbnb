9d0e4102e77a0cfbe96c197543924e4c0c3011c3
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-03-26 13:24:36 +0000

    written an all method for requests and corresponding tests

--- lib/requests.rb
+++ lib/requests.rb
@@ -13,6 +13,20 @@ attr_reader :id, :listing_user_id, :requester_user_id, :listing_id, :name, :desc
     @dates_booked = dates_booked
   end
 
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+
+    result = connection.exec("SELECT * FROM requests")
+
+    result.map do |request|
+      Requests.new(id: result[0]['id'], listing_user_id: result[0]['listing_user_id'], requester_user_id: result[0]['requester_user_id'], listing_id: result[0]['listing_id'], name: result[0]['name'], description: result[0]['description'], price: result[0]['price'], dates_booked: result[0]['dates_booked'])
+    end
+  end
+
   def self.create(listing_user_id:, requester_user_id:, listing_id:, name:, description:, price:, dates_booked:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'makersbnb_test')
